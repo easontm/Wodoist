c7df3afa0e1160b508a457383af555d58e3036a7
Tyler Eason <easontm@gmail.com>
2019-05-22 01:21:39 -0700

    API instance handling is now a singleton per module instance (should only be one anyway). create_project functional, supports parent.

--- src/todoist_handler.py
+++ src/todoist_handler.py
@@ -1,31 +1,76 @@
 import os.path
 from todoist.api import TodoistAPI
+import logging
+
+logger = logging.getLogger(__name__)
+
+'''
+def get_api():
+  global GLOBAL_API:
+  
+  ...
+  return GLOBAL_API
+'''
 
 token_location = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     '.todoist_token'
 )
+logger.info('Token location: %s' % token_location)
 
 if os.path.exists(token_location):
     with open(token_location) as f:
         token = f.readline().strip()
+logger.info('Token: %s' % token)
 
 
 class TodoistHandler(object):
-    @staticmethod
-    def get_api():
+
+    def __init__(self):
+        self.api = None
+
+    def get_api(self):
         """
         Syncs the API call instance if it exists, otherwise creates one
         :return:
         API instance
         """
-        api_conn = TodoistAPI(token)
-        api_conn.sync()
-        return api_conn
+        if self.api:
+            return self.api
+        self.api = TodoistAPI(token)
+        self.api.sync()
+        return self.api
 
-    @staticmethod
-    def submit_todoist(action):
-        api = TodoistHandler.get_api()
+    def submit_todoist(self, action):
+        api = self.get_api()
         project1 = api.projects.add(action)
         api.commit()
         # task1 = api.items.add('Task1', project_id=project1['id'])
+
+    def get_project_by_name(self, name, api=None):
+        """
+        Returns the first found project matching name
+        ~~ If there are duplicates it can't tell! ~~
+        :param name: str - project name search
+        :param api: an api instance
+        :return: project object
+        """
+        if api is None:
+            api = self.get_api()
+        for p in api.state['projects']:
+            if p['name'] == name:
+                return p
+
+    def create_project(self, name, parent=None):
+        """
+        Creates a project and commits the change
+        :param name: str name of the project you want to make
+        :param parent: optional - str name of the parent project
+        :return:
+        """
+        api = self.get_api()
+        parent_id = None
+        if parent is not None:
+            parent_id = self.get_project_by_name(parent, api)['id']
+        api.projects.add(name, parent_id=parent_id)
+        api.commit()
